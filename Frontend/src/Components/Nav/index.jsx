9a3ebcf218391ffd94d3f4f06f9371baa7b8ea5c
kennethk89 <kennethk89@gmail.com>
2018-03-22 22:35:49 -0700

    last commit

--- Frontend/src/Components/Nav/index.jsx
+++ Frontend/src/Components/Nav/index.jsx
@@ -5,7 +5,7 @@ import './nav.css'
 class Nav extends Component {
     render() {
         return (
-            <div className="container-fluid Nav">
+            <div className="container Nav">
                 <header className="App-header">
                     <Link to='/'><img id="Logo" src='/Images/Logo/lolologo.png' alt="sorry" /></Link>
                 </header>
